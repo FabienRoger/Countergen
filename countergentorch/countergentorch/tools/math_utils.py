fb3b3dba709ee64b75ce4cd5de1a0bf261f028bd
Fabien Roger <efab.roger@gmail.com>
2022-09-30 22:00:08 +0200

    Remove deprecitaed test

--- countergentorch/countergentorch/tools/math_utils.py
+++ countergentorch/countergentorch/tools/math_utils.py
@@ -10,6 +10,6 @@ def orthonormalize(dir: torch.Tensor, dirs: torch.Tensor) -> torch.Tensor:
     new_dir = dir - torch.einsum("n, n h -> h", inner_products, dirs)
     new_dir /= torch.linalg.norm(new_dir)
 
-    torch.testing.assert_allclose(torch.einsum("n h, h -> n", dirs, new_dir), torch.zeros(len(dirs)))
+    # torch.testing.assert_allclose(torch.einsum("n h, h -> n", dirs, new_dir), torch.zeros(len(dirs)))
 
     return new_dir
