d461ab99b2f245dd11c3e076828f7e1c70de0d2f
Fabien Roger <efab.roger@gmail.com>
2022-10-09 11:54:34 +0200

    Remove duplicate

--- countergentorch/countergentorch/evaluation/generative_models.py
+++ countergentorch/countergentorch/evaluation/generative_models.py
@@ -5,6 +5,7 @@ import torch
 from countergen.tools.math_utils import perplexity
 from countergentorch.tools.utils import concat_dicts, get_device, get_gpt_tokenizer, remove_last_tok
 from countergen.types import Input, ModelEvaluator, Outputs, Performance
+from countergen import get_generative_model_evaluator
 from transformers import BatchEncoding, GPT2LMHeadModel, GPT2Tokenizer
 
 metrics = ["perplexity", "probability"]
@@ -32,36 +33,6 @@ def pt_to_generative_model(model: torch.nn.Module, device: Optional[str] = None)
         return get_correct_logprobs(tokens_inp, token_outs, model)
 
     return gen_model
-
-
-def get_generative_model_evaluator(model: GenerativeModel, metric: str = "probability") -> ModelEvaluator:
-    """Return the ModelEvaluator corresponding to the model & the metric.
-
-    Available metrics: probability & perplexity"""
-
-    def run(inp: Input, out: Outputs) -> Performance:
-        if len(out) == 0:
-            raise ValueError("Expected output should be provided for gpt models")
-        if len(inp) == 0:
-            raise ValueError("Empty inputs are forbidden for gpt models.")
-
-        correct_log_probs_list = model(inp, out)
-
-        total_prob: float = 0
-        total_log_prob: float = 0
-        number_of_toks: int = 0
-        for correct_log_probs in correct_log_probs_list:
-            total_prob += exp(sum(correct_log_probs))
-            number_of_toks += len(correct_log_probs)
-            total_log_prob += sum(correct_log_probs)
-
-        if metric == "perplexity":
-            return exp(-total_log_prob / number_of_toks)
-        if metric == "probability":
-            return total_prob
-        raise ValueError(f"{metric} is not a valid metric. Choose one in {metrics}.")
-
-    return run
 
 
 def get_correct_logprobs(
